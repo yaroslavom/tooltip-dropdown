5c955d4cc727b109001ab2f12bcc63a59246b623
Yaroslavom <yarykomm@gmail.com>
2021-10-13 00:44:56 +0300

    ToolTip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const ToolTipText = styled("span")({
+  visibility: "hidden",
+  width: "auto",
+  backgroundColor: "#000",
+  color: "#fff",
+  textAlign: "center",
+  borderRadius: "6px",
+  padding: "5px 5px",
+  position: "absolute",
+  zIndex: 1,
+  top: "150%",
+  left: "50%",
+  marginLeft: "-100px",
+  ":after": {
+    content: '""',
+    position: "absolute",
+    bottom: "100%",
+    left: "50%",
+    marginLeft: "-5px",
+    borderWidth: "5px",
+    borderStyle: "solid",
+    borderColor: "transparent transparent black  transparent"
+  }
+});
+
+const ToolTip = styled("div")({
+  position: "relative",
+  display: "inline-block",
+  ":hover span": {
+    visibility: "visible"
+  }
+});
 
 function App() {
+  const item = '12345678901234567890123456789'
+
+  const textEllipsis = (str) => {
+    const maxLength = 25;
+    const ellipsis = '...';
+    if (str.length >= maxLength) {
+      return str.slice(0, maxLength - ellipsis.length) + ellipsis;
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ margin: '100px' }}>
+      <ToolTip>
+        {textEllipsis(item)}
+        <ToolTipText>{item}</ToolTipText>
+      </ToolTip>
     </div>
   );
 }
